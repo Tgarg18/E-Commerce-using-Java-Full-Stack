f6b8e4b3397dda5f9cba1a48769a5058861c5929
Tgarg18 <tushargarg70091@gmail.com>
2024-12-19 17:20:34 +0530

    frontend-setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="bg-green-300 text-green-900 text-center">Hello</div>
+    </>
+  )
+}
+
+export default App
