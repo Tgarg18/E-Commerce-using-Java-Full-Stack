4a897dd83622272cb034bd02561aacaf249762b2
Tgarg18 <tushargarg70091@gmail.com>
2024-12-22 03:12:30 +0530

    filterpage-productcardpage-added

--- frontend/src/customer/components/Product/ProductCard.jsx
+++ frontend/src/customer/components/Product/ProductCard.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import './ProductCard.css'
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
     return (
         <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
-            
+
             <div className='h-[20rem]'>
-                <img className='h-full w-full object-cover object-center-top' draggable={false} src="https://rukminim1.flixcart.com/image/612/612/xif0q/t-shirt/1/d/2/s-3813-the-dry-state-original-imagcfh833gdjeem-bb.jpeg?q=70" alt="" />
+                <img className='h-full w-full object-cover object-center-top' draggable={false} src={product.imageUrl} alt={product.title} />
             </div>
 
             <div className='textPart bg-white p-3'>
                 <div>
-                    <p className='font-bold opacity-60'>THE DRY STATE</p>
-                    <p>Women Printed Round Neck Pure Cotton White T-Shirt</p>
+                    <p className='font-bold opacity-60'>{product.brand}</p>
+                    <p>{product.title}</p>
                 </div>
                 <div className='flex items-center space-x-2'>
-                    <p className=' font-semibold'>₹399</p>
-                    <p className='line-through opacity-50'>₹1399</p>
-                    <p className='text-green-600 font-semibold'>70% off</p>
+                    <p className=' font-semibold'>₹{product.discountedPrice}</p>
+                    <p className='line-through opacity-50'>₹{product.price}</p>
+                    <p className='text-green-600 font-semibold'>{product.discountPersent}% off</p>
                 </div>
             </div>
 
