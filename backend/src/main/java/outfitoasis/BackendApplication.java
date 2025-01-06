5b0aeea6557bd37fddc976ff5ae38be9c7d0986b
Tgarg18 <tushargarg70091@gmail.com>
2025-01-06 15:00:30 +0530

    setting-.env-path

--- backend/src/main/java/outfitoasis/BackendApplication.java
+++ backend/src/main/java/outfitoasis/BackendApplication.java
@@ -1,22 +1,38 @@
 package outfitoasis;
 
 import io.github.cdimascio.dotenv.Dotenv;
+
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().directory("./backend/.env").load();
+
+		String workingDirectory = Paths.get(".").toAbsolutePath().normalize().toString();
+		String envFilePath;
+
+		if(workingDirectory.endsWith("backend"))
+			envFilePath = workingDirectory + "/.env";
+		else
+			envFilePath = workingDirectory + "/backend/.env";
+
+		Dotenv dotenv = Dotenv.configure()
+				.directory(envFilePath)
+				.load();
+
 		System.setProperty("DB", dotenv.get("DB"));
 		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
 		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		System.setProperty("SECRET_KEY", dotenv.get("SECRET_KEY"));
+
 		SpringApplication.run(BackendApplication.class, args);
+
 	}
 
 }
